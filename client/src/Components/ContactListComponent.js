b40122e30e7c036aea5a5aa32c56fcf3f4149fc7
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-08-03 00:33:48 +0530

    completed the header part of the ContactList section using styled components

--- client/src/Components/ContactListComponent.js
+++ client/src/Components/ContactListComponent.js
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+height: 100%;
+flex: 1.6;
+`;
+
+const ProfileInfoDiv = styled.div`
+display: flex;
+flex-direction: column;
+background: #ededed;
+padding: 10px;
+`;
+
+const ProfileImage = styled.img`
+width: 32px;
+height: 32px;
+border-radius: 50%;
+`;
+
+
 const ContactListComponent = () => {
-    return ('ContactListComponent');
+    return <Container>
+        <ProfileInfoDiv>
+            <ProfileImage src="/profile/men1.png"/>
+        </ProfileInfoDiv>
+        'ContactListComponent'</Container>;
 }
 
 export default ContactListComponent;
--- client/src/Components/ConversationComponent.js
+++ client/src/Components/ConversationComponent.js
@@ -1,5 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+height: 100%;
+flex: 3;
+background: #f6f7f8;
+`;
+
+
 const ConversationComponent = () => {
-    return ('ConversationComponent');
+    return <Container>'ConversationComponent'</Container>;
 }
 
 export default ConversationComponent;
