2f75869c3bc3f2742c3f36558403920397dc05fb
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-08-03 01:31:50 +0530

    successfully completed the UI part of the ContactList component by completing ContactComponent

--- client/src/Components/ContactListComponent.js
+++ client/src/Components/ContactListComponent.js
@@ -22,6 +22,7 @@ border-radius: 50%;
 `;
 
 const SearchBox = styled.div`
+display: flex;
 background: #f6f6f6;
 padding: 10px;
 `;
@@ -47,6 +48,59 @@ border: none;
 font-size: 15px;
 `;
 
+const ContactItem = styled.div`
+display: flex;
+flex-direction: row;
+border-bottom: 1px solid #f2f2f2;
+background: white;
+cursor: pointer;
+padding: 15px 12px;
+`;
+
+const ProfileIcon = styled(ProfileImage)`
+width: 38px;
+height: 38px;
+`;
+
+const ContactInfo = styled.div`
+display: flex;
+flex-direction: column;
+width: 100%;
+margin: 0 12px;
+`;
+
+const ContactName = styled.span`
+width: 100%;
+font-size: 16px;
+color: black;
+`;
+
+const MessageText = styled.span`
+width: 100%;
+font-size: 14px;
+margin-top: 3px;
+color: rgba(0,0,0,0.8);
+`;
+
+const MessageTime = styled.span`
+font-size: 12px;
+margin-right: 10px;
+color: rgba(0,0,0,0.45);
+white-space: nowrap;
+`;
+
+
+const ContactComponent = () => {
+    return <ContactItem>
+        <ProfileIcon src="/profile/men1.png"/>
+        <ContactInfo>
+          <ContactName>Aniket Darekar</ContactName>
+          <MessageText>Hey man!</MessageText>
+        </ContactInfo>
+        <MessageTime>05.32 PM</MessageTime>
+        </ContactItem>;
+} 
+
 
 const ContactListComponent = () => {
     return (
@@ -61,6 +115,12 @@ const ContactListComponent = () => {
             <SearchInput placeholder="Search or start new chat"/>
           </SearchContainer>
         </SearchBox>
+
+        <ContactComponent />
+        <ContactComponent />
+        <ContactComponent />
+        <ContactComponent />
+        <ContactComponent />
 
         </Container>
     );
