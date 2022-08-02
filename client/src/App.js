842d62835f06d83ab25559c194d8118268ccc5bd
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-08-02 23:42:25 +0530

    project initialized and install all necessary dependencies

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+display: flex;
+flex-direction: row;
+height: 100vh;
+width: 100%;
+background: #f8f9fb;
+`
+
+function App() {
+  return (
+    <Container>
+        <h1>Hello</h1>
+    </Container>
+  );
+}
+
+export default App;
