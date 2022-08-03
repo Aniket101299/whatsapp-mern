c6b9be9f3d98169bac4df3277dbf69454605201d
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-08-03 10:19:24 +0530

    added emoji picker and able to send message on enter press that functionality added & successfully completed frontend part

--- client/src/Components/ConversationComponent.js
+++ client/src/Components/ConversationComponent.js
@@ -1,4 +1,6 @@
+import React, { useState } from "react";
 import styled from "styled-components";
+import Picker from "emoji-picker-react";
 import { SearchContainer, SearchInput } from "./ContactListComponent";
 import { messagesList } from "../mockData";
 
@@ -63,9 +65,34 @@ font-size: 14px;
 border-radius: 4px;
 `;
 
+
+
 const ConversationComponent = (props) => {
 
     const { selectedChat } = props;
+    const [text, setText] = useState("");
+    const [pickerVisible, togglePicker] = useState(false);
+    const [messageList, setMessageList] = useState(messagesList);
+
+    const onEmojiClick = (event, emojiObj) => {
+        setText(text + emojiObj.emoji);
+        togglePicker(false);
+    };
+
+    const onEnterPress = (event) => {
+        if(event.key === "Enter") {
+            const messages = [...messageList];
+            messages.push({
+                id: 0,
+                messageType: "TEXT",
+                text,
+                senderID: 0,
+                addedOn: "12:09 PM",
+            });
+            setMessageList(messages);
+            setText("");
+        }
+    };
 
     return (
     <Container>
@@ -75,7 +102,7 @@ const ConversationComponent = (props) => {
         </ProfileHeader>
 
         <MessageContainer>
-            {messagesList.map((messageData) => (
+            {messageList.map((messageData) => (
                <MessageDiv isYours={messageData.senderID === 0}>
                   <Message isYours={messageData.senderID === 0}>
                     {messageData.text}
@@ -86,8 +113,19 @@ const ConversationComponent = (props) => {
 
         <ChatBox>
           <SearchContainer>
-            <EmojiImage src="/emojiimg.svg" />
-            <SearchInput placeholder="Type a message"/>
+            {pickerVisible && (
+            <Picker 
+            pickerStyle={{ position: "absolute", bottom: "60px" }}
+            onEmojiClick={onEmojiClick} 
+            />
+            )}
+            <EmojiImage src="/emojiimg.svg" onClick={() => togglePicker(!pickerVisible)} />
+            <SearchInput 
+            placeholder="Type a message" 
+            value={text}
+            onKeyDown={onEnterPress}
+            onChange={(e) => setText(e.target.value)}
+            />
           </SearchContainer>
         </ChatBox>
         </Container>
